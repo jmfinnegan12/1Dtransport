181395a15016029789e26a0da15ca12643d5978d
Jim Finnegan <jmfinnegan12@gmail.com>
2020-05-06 19:41:59 -0400

    Added initial files
    Includes analytical results and incorrect FDM results

---
+++ 1D Finite Difference.py
@@ -0,0 +1,55 @@
+# Groundwater Modeling Coding Assignment #2
+# Jim Finnegan
+# 1D Transport Equation
+# Finite Difference Method
+import numpy as np
+from scipy.sparse import diags
+from matplotlib import pyplot as plt
+
+# user inputs
+D = float(input('Enter D (m^2/d): '))
+R = float(input('Enter R: '))
+
+# other parameters
+v, L, dx, t, dt = 0.1, 200, 2, 400, 10
+# matrix dimensions
+rows = int(t / dt) + 1
+cols = int(L / dx) + 1
+
+# initial conditions
+C0 = np.zeros(cols)
+C = np.zeros((rows, cols))
+C[:, 0] = 1 # boundary condition: C/C0 = 1 at x=0
+
+# simplified variables from central difference derivation
+G = (D * dt) / (2 * R * dx**2)
+H = (v * dt) / (4 * R * dx)
+lam_1, lam_2, lam_3, lam_4 = G + H, 2*G + 1, G - H, 2*G - 1
+
+# CENTERED DIFFERENCE SCHEME
+#   Left hand side - k+1
+A_diagonals = [np.ones(cols-1)*lam_1, np.ones(cols)*-lam_2, np.ones(cols-1)*lam_3]
+A = diags(A_diagonals, offsets=[-1, 0, 1], shape=(cols, cols)).toarray()
+#   Right hand side - k
+B_diagonals = [np.ones(cols-1)*-lam_1, np.ones(cols)*lam_4, np.ones(cols-1)*-lam_3]
+B = diags(B_diagonals, offsets=[-1, 0, 1], shape=(cols, cols)).toarray()
+
+
+for k in range(1, rows):
+    b = np.dot(B, C[k-1, :])            # solve RHS
+    C[k, :] = np.linalg.solve(A, b)     # solve LHS
+    C[k][0] = 1                         # enforce boundary condition
+print('Centered difference results: ')
+print(C)
+
+x = np.linspace(0, L, num=cols)
+plt.plot(x, C[0, :])
+plt.plot(x, C[10, :])
+plt.plot(x, C[20, :])
+plt.plot(x, C[30, :])
+plt.plot(x, C[40, :])
+
+plt.xlabel('distance (m)')
+plt.ylabel('C/C0')
+plt.legend(['0 days', '100 days', '200 days', '300 days', '400 days'])
+plt.show()
---
+++ Analytical Solution.py
@@ -0,0 +1,46 @@
+# Groundwater Modeling Coding Assignment #2
+# Jim Finnegan
+# 1D Transport Equation
+# Analytical Solution
+
+import numpy as np
+from math import exp, sqrt
+from scipy.special import erfc
+from matplotlib import pyplot as plt
+
+# initial conditions
+# for R = 1
+v = 0.1
+D = int(input('Enter D (m^2/d): '))
+L, dx = 200, 2
+dist = np.linspace(2, L, num=int(L/dx))
+dist = [int(x) for x in dist]
+# print(dist)
+t, dt = 400, 10
+time = np.linspace(10, t, num=int(t/dt))
+time = [int(t) for t in time]
+# print(time)
+
+# initialize grid for C
+# x is distance (one column is 2 ft), y is time (one row is 10 days)
+C = np.zeros((len(time)+1, len(dist)+1))
+C[:, 0] = 1     # boundary condition: C/C0 = 1 at x=0
+
+# calculate C using analytical solution for all x>0
+for x in range(len(dist)):
+    for t in range(len(time)):
+        C[t+1][x+1] = (1/2)*(exp(v*dist[x]/D)*erfc((dist[x]+v*time[t])/(2*sqrt(D*time[t])))+erfc((dist[x]-v*time[t])/(2*sqrt(D*time[t]))))
+
+# plot
+dist.insert(0, 0)
+plt.plot(dist, C[0, :])
+plt.plot(dist, C[10, :])
+plt.plot(dist, C[20, :])
+plt.plot(dist, C[30, :])
+plt.plot(dist, C[40, :])
+
+plt.title('Analytical Solution')
+plt.xlabel('distance (m)')
+plt.ylabel('C/C0')
+plt.legend(['0 days', '100 days', '200 days', '300 days', '400 days'])
+plt.show()
